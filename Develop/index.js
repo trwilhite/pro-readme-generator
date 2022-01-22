4635c7015ba485c5a8d58f064388628672fe1861
Tamara Wilhite <wilhite.tamara6452@gmail.com>
2022-01-22 16:12:42 -0500

    created question array

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,54 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use.'
+    },
+    {
+        type: 'input',
+        name: 'contributions',
+        message: 'Provide guidelines for other users to contribute to your project.'
+    },
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'Provide examples on how to run tests on your project.'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'Mozilla Public License 2.0'],
+        message: 'What license does your project have?'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
