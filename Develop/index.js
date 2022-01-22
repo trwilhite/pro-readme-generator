9a703175b0e38bcbf775c4552972213825bfe3cc
Tamara Wilhite <wilhite.tamara6452@gmail.com>
2022-01-22 18:11:40 -0500

    created initial readme template

--- Develop/index.js
+++ Develop/index.js
@@ -54,10 +54,11 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+};
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -12,8 +12,38 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  * [Description] (#description)
+  * [Installation] (#installation)
+  * [Usage] (#usage)
+  * [Contributions] (#contributions)
+  * [Testing] (#testing)
+  * [License] (#license)
+  * [Questions] (#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contributions
+  ${data.contributions}
+
+  ## Testing
+  ${data.testing}
+
+  ##License
+  This project has been licensed under the following: ${data.license}
+
+  ## Questions
+  For any questions regarding this or any other project created by ${data.github}, please contact me at ${data.email}.
 `;
 }
 
